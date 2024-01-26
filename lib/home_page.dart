e446f987d12fd7f66192c466923f0fe8606e8d9f
lampod <lampod@hotmail.com>
2024-01-26 22:26:03 +0700

    Midterm Start project

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class home_page extends StatefulWidget {
+  const home_page({super.key});
+
+  @override
+  _page createState() => _page();
+}
+
+class _page extends State<home_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: 'Test',
+    );
+  }
+}
